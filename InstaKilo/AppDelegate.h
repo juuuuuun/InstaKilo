126012964c392bcc0aee0f2e3ada932d01715684
Jun Oh <kyungjunjoh@gmail.com>
2019-01-23 11:33:51 -0800

    Initial Commit

---
+++ InstaKilo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  InstaKilo
+//
+//  Created by Jun Oh on 2019-01-23.
+//  Copyright © 2019 Jun Oh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ InstaKilo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  InstaKilo
+//
+//  Created by Jun Oh on 2019-01-23.
+//  Copyright © 2019 Jun Oh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
