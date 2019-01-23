6382e0a8f6e4d9331acbfa8ef367728bb18902bd
Jun Oh <kyungjunjoh@gmail.com>
2019-01-23 11:41:14 -0800

    Added photos to display

---
+++ InstaKilo/PhotoCollectionViewCell.m
@@ -0,0 +1,13 @@
+//
+//  PhotoCollectionViewCell.m
+//  InstaKilo
+//
+//  Created by Jun Oh on 2019-01-23.
+//  Copyright © 2019 Jun Oh. All rights reserved.
+//
+
+#import "PhotoCollectionViewCell.h"
+
+@implementation PhotoCollectionViewCell
+
+@end
